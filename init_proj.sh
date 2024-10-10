b249a2512fbc87c5de2199738da9991468306e09
IvEd <ivedamenko@outlook.com>
2024-10-10 22:37:59 +0300

    init proj upd

--- init_proj.sh
+++ init_proj.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 mkdir src
-mkdir bin
-touch CMakeLists.txt
+
+wget https://raw.githubusercontent.com/IvEd123/cpp_project_template/refs/heads/main/scripts/index.sh -O index.sh
+chmod +x index.sh
+
+wget https://raw.githubusercontent.com/IvEd123/cpp_project_template/refs/heads/main/templates/CMakeLists.txt.root -O CMakeLists.txt
+
+cd src
+wget https://raw.githubusercontent.com/IvEd123/cpp_project_template/refs/heads/main/templates/CMakeLists.txt.src -O CMakeLists.txt
+
